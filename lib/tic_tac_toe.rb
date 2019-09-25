061de9fa84899ac42e02a1b77e1e054ec9e0ab42
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-25 15:55:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,8 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-  (0..8).cover?(index) && !position_taken?(index)
+  index.between?(0,8) && !position_taken?(index)
+  # (0..8).covers?(index)
 end
 
 def turn
