2c9ede371a1acc21665d2b51794c05187579c43b
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-25 15:30:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,12 +75,11 @@ end
 
 def won?
   WIN_COMBINATIONS.any? do |combo|
-    if position_taken?(combo[0])
-      @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+    if position_taken?(combo[0]) == @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
       return combo
     end
 end
- false
+# false
 end
 
 def full?
