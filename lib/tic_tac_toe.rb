5cb22807cd125aba732601ca41de5e7d97abd837
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-25 15:27:07 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,119 @@
+require 'pry'
+
+class TicTacToe
+  attr_accessor :board
+
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#    board
+  end
+
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+  index
+end
+
+# player = "X"
+def move(index, player)
+  @board[index] = player
+end
+
+def position_taken?(index)
+  if @board[index] == "" || @board[index] == " " || @board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(index)
+  (0..8).cover?(index) && position_taken?(index)
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, player = current_player)
+    display_board
+  else
+    turn
+  end
+end
+
+def turn_count
+  board.count("X") + board.count("O")
+end
+
+def current_player
+  if turn_count.even?
+    return "X"
+  else
+    return "O"
+  end
+end
+
+###############
+def won?
+  # xs = board.each_index.select{|i| board[i] == 'X'}
+  # os = board.each_index.select{|i| board[i] == 'O'}
+  WIN_COMBINATIONS.any? do |combo|
+    if position_taken?(combo[0])
+      @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+      return combo
+    end
+end
+ false
+end
+
+    # binding.pry
+
+    # if xs.include?(a.all?) || os.include?(a.all?)
+    #   win << a
+    # if  xs == a || os == a
+    #   win = a
+    # else
+    #   win = nil
+#   end
+# # win
+# end
+
+def full?
+  @board.all? {|e| e != " "}
+end
+
+def draw?
+  full? && !won?
+end
+
+def over?
+  full? || draw?
+end
+
+def winner?
+end
+
+def player
+end
+
+end
